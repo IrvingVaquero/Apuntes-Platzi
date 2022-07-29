dd86e7f234004d3ff72f3e96db6ac8e5050bde56
Irving Vaquero <ivaqueroflores@gmail.com>
2022-07-29 01:03:56 -0600

    Cree documento para apuntes de Intro a Backend

---
+++ Apuntes-Introduccion-Backend.py
@@ -0,0 +1,37 @@
+
+""" 
+    !   Framework vs Libreria
+    ?   Framework
+    *   Marco de trabajo o reglas
+    %   Angular
+    ?   Libreria
+    *   Codigo echo por otra persona
+    %   React
+
+    !   Como se conecta?
+    *   Se realiza mediante la API Interfas de programacion de aplicaciones
+    
+    SOAP
+    Mueve la informacion mediante XML
+    REST
+    Mueve la informamcion mediante JSON 
+
+    !   HTTP
+    ?   Protocolo de tranferencia de hipertexto
+    ?   
+    *   Request y Response
+    ?   Tipo de errores
+    %   https://plataforma.josedomingo.org/pledin/cursos/flask/curso/u01/img/dia2.png
+
+    !   IAAS
+    *   Control como el CPU
+    *   RAM
+    *   SSD
+
+    !   PAAS
+    *   Actualiza las tecnologias que se utiliza como base de datos
+
+    !   SAAS
+    *   Es una aplicacion que nos da la oportunidad de crear apps [No Code]
+
+"""
