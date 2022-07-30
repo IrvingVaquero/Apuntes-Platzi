b5c1209beb01871ea08a5a22331a164a29b2181e
Irving Vaquero <ivaqueroflores@gmail.com>
2022-07-29 19:40:18 -0600

    Agrege documentos de clases

---
+++ Class_Example/Java/Payment.java
@@ -0,0 +1,6 @@
+package Class_Example.Java;
+
+class Payment {
+    Integer id;
+    
+}
---
+++ Class_Example/Java/Route.java
@@ -0,0 +1,8 @@
+package Class_Example.Java;
+import java.util.ArrayList;
+class Route {
+    Integer id;
+    ArrayList<Double> start;
+    ArrayList<Double> end;
+    
+}
