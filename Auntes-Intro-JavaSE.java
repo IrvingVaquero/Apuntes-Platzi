fdc663df6c184b950bf344730dcb88107c3ade3e
Irving Vaquero <ivaqueroflores@gmail.com>
2022-08-03 14:38:48 -0600

    Primeros apuntes SE

---
+++ Auntes-Intro-JavaSE.java
@@ -0,0 +1,27 @@
+/* 
+ * James Gosling -- Creador
+ * JAVA SE - La BASE Y SIN
+ * JDK - Java Development Kit -- 1995
+ * Java Runtime Enviroment - Es la maquina virtual
+ * Compilador java - Es lo que lo traduce para que lo entienda la maquina
+ * Api de desarrollo - Es la base para poder crear lo nuestro
+ * LTS - cada 3 años de soporte 
+ * IDE - entorno de desarrrollo integrado
+ % Es un entorno de codigo como lo son los editores de codigo, compilador, depurador, constructor de interfaz grafica 
+ * main es el punto de entrada al programa
+ * public static void main (String[] args)
+ * 
+ ? System.out.println
+ * Mando en consola
+ *
+ ? Etapas de la programacion
+ * Creacion de archivo .java -- Codigo fuente
+ * Run - Se activa el compildor javac
+ * Byte code class - Contiene lo que entiende la maquina
+ * JVM - Crea el efecto multipltforma
+ * 
+ ! Variable
+ ? Tipo_De_Dato nombre = Asignacion;
+ 
+
+ */
