317d13aa506ec66e1227dc658f8761d3147b49b6
Irving Vaquero <ivaqueroflores@gmail.com>
2022-08-10 00:26:00 -0600

    Funcionamieno de los namespace

--- apuntes-fundamento-c.c
+++ apuntes-fundamento-c.c
@@ -1,5 +1,12 @@
 /* 
-
+    !   Funcionamiento de la namespace:
+    *   Abarca de forma global en el programa
+    %   >Clase
+    %   >>Method
+    %   >>Method
+    %   >CLase
+    %   >>Method
+    %   >>Method
 
 
 
