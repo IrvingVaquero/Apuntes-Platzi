d30e195ea7cc5e3def9fd7e0b3be0d0c67e22c51
Irving Vaquero <ivaqueroflores@gmail.com>
2022-08-11 18:23:25 -0600

    Agrege el segundo documento de apuntes para netcore

---
+++ apunte-netcore.cs
@@ -0,0 +1,11 @@
+/* 
+    !   Fuertemente tipado
+    !   Funcional
+    !   POO basada en clases y componenetes
+
+    !   Clases y objetos
+    *   Un objeto es una instancia de una clase
+    *   Una clase es la plantilla
+
+
+ */
