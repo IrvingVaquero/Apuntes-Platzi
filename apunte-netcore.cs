b96bdc1dd6a64aac7102fd976d28d65d9db8fe74
Irving Vaquero <ivaqueroflores@gmail.com>
2022-08-12 23:27:31 -0600

    Lista de valores y clases en .net core

--- apunte-netcore.cs
+++ apunte-netcore.cs
@@ -7,5 +7,38 @@
     *   Un objeto es una instancia de una clase
     *   Una clase es la plantilla
 
+    ! Ejemplo de clase vr 6.0
+
+    var escuela = new CoreEscuela();
+    escuela.nombre = "Platzi";
+    escuela.direccion = "Tecnologico";
+    escuela.anioFundacion = 2008;
+    Console.WriteLine("Timbre");
+    escuela.Timbrar();
+
+    class CoreEscuela{
+        public string nombre;
+        public string direccion;
+        public int anioFundacion;
+
+        public void Timbrar(){
+            Console.Beep(10000, 3000);
+        }
+
+    }
+
+    !   Ejemplo lista de valores
+
+    namespace CoreEscuela.Entidades
+{
+    public enum TiposEscuela
+    {
+        Primaria, Secundaria, PreEscolar
+    }
+
+}
+
+
+
 
  */
